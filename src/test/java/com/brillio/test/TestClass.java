23b32be4925dc3313f3f46b0c55b61ea605ab789
kunal.mazumdar <kunal.mazumdar@CLBBLR-7037.BRILLIO.COM>
2015-06-19 15:18:22 +0530

    Rolled back to Chrome Driver.

--- src/test/java/com/brillio/test/TestClass.java
+++ src/test/java/com/brillio/test/TestClass.java
@@ -7,9 +7,7 @@ import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
@@ -18,25 +16,25 @@ import org.testng.annotations.Test;
 
 public class TestClass {
 
-	static WebDriver driver;
+	static WebDriver driver = null;
 	
 	@BeforeTest
 	public void setUp(){
-		/*System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
-		System.out.println("Chrome Browser Instance Invoked !!");*/
-		DesiredCapabilities caps = new DesiredCapabilities();
+		System.out.println("Chrome Browser Instance Invoked !!");
+		/*DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true); 
         caps.setCapability("takesScreenshot", true);
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "drivers/phantomjs.exe");          
-        caps.setCapability("phantomjs.page.customHeaders.Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-        caps.setCapability("phantomjs.page.customHeaders.Accept-Language", "en-GB,en-US;q=0.8,en;q=0.6");
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "drivers/phantomjs.exe");*/          
+        //caps.setCapability("phantomjs.page.customHeaders.Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        //caps.setCapability("phantomjs.page.customHeaders.Accept-Language", "en-GB,en-US;q=0.8,en;q=0.6");
+        /*caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {
             "--web-security=false",
             "--ssl-protocol=any",
             "--ignore-ssl-errors=true"
-        });
-        driver  = new PhantomJSDriver(caps);
+        });*/
+        //driver  = new PhantomJSDriver(caps);
 	}
 	
 	@Test
@@ -59,6 +57,7 @@ public class TestClass {
 			WebElement elem1 = driver.findElement(By.xpath("//*[@id='threshold']"));
 			elem1.clear();
 			elem1.sendKeys("90");
+			Assert.assertEquals(elem1.getAttribute("id"), "threshold");
 			
 			System.out.println("Form Data Filled !!");
 			Thread.sleep(4000);
@@ -67,13 +66,14 @@ public class TestClass {
 			System.out.println("Job has been submitted !!");
 			Thread.sleep(5000);
 			
-			Assert.assertEquals(elem1.getAttribute("id"), "threshold");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-			driver.close();
+			if(driver != null)
+				driver.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-			driver.close();
+			if(driver != null)
+				driver.close();
 		}
 	}
 	
